4e3860001ace99e7cd86cd465fbc06babebde75e
Mystic-Mirage <mm@m10e.net>
2020-09-19 15:41:19 +0300

    Add preferences as a fragment

--- app/src/main/java/com/lesivka/keyboard/MainActivity.java
+++ app/src/main/java/com/lesivka/keyboard/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getFragmentManager().beginTransaction().add(R.id.main, new Settings()).commit();
         checkEnabled();
     }
 
